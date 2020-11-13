ca3e0a2a7a5a611f5cae918fba127f3f21f59e91
aleem-ahmed <opall.io2020@gmail.com>
2020-11-13 15:54:11 -0500

    Improved Activities Page

--- client/src/pages/activity/index.vue
+++ client/src/pages/activity/index.vue
@@ -11,60 +11,54 @@
 							class="list-group-item bg-dark"
 						>
 							<div class="row">
-								<!-- Created User -->
-								<div
-									v-if="activity.type == 'user'"
-									class="col-sm-6 text-success"
-								>
-									<h5>
-										<img
-											:src="activity.user.profileImg"
-											alt="x"
-											class="mr-2"
-											style="max-width: 34px;"
-										>
-										{{ activity.user.username }} joined the site!
-									</h5>
-								</div>
+								<!-- Details Section -->
+								<div class="col-sm-8">
+									<!-- Created User -->
+									<div v-if="activity.type == 'user'" class="row text-success">
+										<div class="col m-0 p-0" style="max-width: 35px;">
+											<img
+												:src="activity.user.profileImg"
+												class="w-100"
+											>
+										</div>
+										<h5 class="col">
+											{{ activity.user.username }} joined the site!
+										</h5>
+									</div>
 
-								<!-- Created Post -->
-								<div
-									v-if="activity.type == 'post'"
-									class="col-sm-6 text-primary"
-								>
-									<h5>
-										<img
-											:src="activity.post.user.profileImg"
-											alt="x"
-											class="mr-2"
-											style="max-width: 34px;"
-										>
-										{{ activity.post.user.username }}
-										created post: 
-										{{ activity.post.title }}
-									</h5>
-								</div>
-
-								<!-- Created Comment -->
-								<div
-									v-if="activity.type == 'comment'"
-									class="col-sm-6 text-primary"
-								>
-									<h5>
-										<img
-											:src="activity.comment.user.profileImg"
-											alt="x"
-											class="mr-2"
-											style="max-width: 34px;"
-										>
-										{{ activity.comment.user.username }}
-										created a comment in
-										{{ activity.comment.post.title }}
-									</h5>
+									<!-- Created Post -->
+									<div v-if="activity.type == 'post'" class="row text-primary">
+										<div class="col m-0 p-0" style="max-width: 35px;">
+											<img
+												:src="activity.post.user.profileImg"
+												class="w-100"
+											>
+										</div>
+										<h5 class="col">
+											{{ activity.post.user.username }}
+											created post: 
+											{{ activity.post.title }}
+										</h5>
+									</div>
+									
+									<!-- Created Comment -->
+									<div v-if="activity.type == 'comment'" class="row">
+										<div class="col m-0 p-0" style="max-width: 35px;">
+											<img
+												:src="activity.comment.user.profileImg"
+												class="w-100"
+											>
+										</div>
+										<h5 class="col">
+											{{ activity.comment.user.username }}
+											created a comment in
+											{{ activity.comment.post.title }}
+										</h5>
+									</div>
 								</div>
 
 								<!-- Timestamp -->
-								<div class="col-sm-6 text-right text-secondary">
+								<div class="col-sm-4 text-right text-secondary">
 									{{ new Date(activity.createdAt).toLocaleString() }}
 								</div>
 							</div>
