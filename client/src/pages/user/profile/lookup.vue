101805a637b2df9fd32c24000d87e8c4fa41f122
aleem-ahmed <aleem.ahmed2072@gmail.com>
2020-12-20 14:20:30 -0500

    Lookup Profile Page

--- client/src/pages/user/profile/lookup.vue
+++ client/src/pages/user/profile/lookup.vue
@@ -1,32 +1,31 @@
 <template>
 	<BContainer>
 		<BRow class="mt-3">
-			<BCol v-if="!error" sm="12" md="8">
+			<BCol v-if="!error" sm="12" md="9" class="mb-3">
 				<BCard bg-variant="dark text-light">
 					<!-- Profile Details -->
-					<BRow>
-						<BCol cols="4">
+					<BRow class="">
+						<BCol cols="3">
+							<h3 class="text-center">{{ user.username }}</h3>
 							<div class="border border-primary rounded">
 								<img
 									:src="user.profileImg"
 									alt="Profile Image Not Available"
-									class="w-100"
+									class="w-100 rounded"
 								>
 							</div>
+
+							<h6 class="mt-2">
+								Joined {{ new Date(user.created_at).toLocaleString() }}
+							</h6>
 						</BCol>
 
-						<BCol cols="8">
+						<BCol cols="9">
 							<BRow class="">
-								<BCol cols="12" md="4">
-									<h3>{{ user.username }}</h3>
-								</BCol>
-							</BRow>
-
-							<BRow class="mt-3">
 								<BCol cols="12" md="4">
 									<BBadge
 										variant="dark"
-										class="w-100 py-1 border border-secondary rounded"
+										class="w-100 mb-2 py-1 border border-secondary rounded"
 									>
 										<h6>Content Score</h6>
 										<h4>--</h4>
@@ -35,7 +34,7 @@
 								<BCol cols="12" md="4">
 									<BBadge
 										variant="dark"
-										class="w-100 py-1 border border-secondary rounded"
+										class="w-100 mb-2 py-1 border border-secondary rounded"
 									>
 										<h6>Total Comments</h6>
 										<h4>{{ data.commentCount }}</h4>
@@ -44,7 +43,7 @@
 								<BCol cols="12" md="4">
 									<BBadge
 										variant="dark"
-										class="w-100 py-1 border border-secondary rounded"
+										class="w-100 mb-2 py-1 border border-secondary rounded"
 									>
 										<h6>Total Posts</h6>
 										<h4>--</h4>
@@ -54,13 +53,30 @@
 
 							<BRow class="mt-3">
 								<BCol cols="12">
-									<p>
-										Joined {{ new Date(user.created_at).toLocaleString() }}
-									</p>
+									Recent Activity
 								</BCol>
 							</BRow>
 						</BCol>
 					</BRow>
+				</BCard>
+			</BCol>
+			<BCol v-if="!error" sm="12" md="3">
+				<BCard bg-variant="dark text-light">
+					<BBadge
+						variant="dark"
+						class="w-100 py-1 rounded"
+					>
+						<h6>Total Friends</h6>
+						<h4>--</h4>
+					</BBadge>
+
+					<BButton variant="outline-success" class="w-100 mt-2">
+						Add Friend
+					</BButton>
+
+					<BButton variant="outline-secondary" class="w-100 mt-2">
+						View Friends
+					</BButton>
 				</BCard>
 			</BCol>
 			<BCol cols="12">
