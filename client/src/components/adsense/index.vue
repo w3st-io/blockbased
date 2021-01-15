a573e6d4a2053283df58e5207e7b8bebe1393652
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-01-15 16:06:47 -0500

    Google Adsense Fix

--- client/src/components/adsense/index.vue
+++ client/src/components/adsense/index.vue
@@ -3,15 +3,15 @@
 		<div class="ad-container">
 			<Adsense
 				data-ad-client="ca-pub-5696881492897672"
-				data-ad-slot="1234567890">
-			</Adsense>
+				data-ad-slot="1234567890"
+			></Adsense>
 
 			<adsense
 				ad-client="ca-pub-5696881492897672"
 				ad-slot="XXXXXXXX"
 				ad-style="display: block"
-				ad-format="auto">
-			</adsense>
+				ad-format="auto"
+			></adsense>
 		</div>
 	</BCard>
 </template>
