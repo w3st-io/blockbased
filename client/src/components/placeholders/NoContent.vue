ba6781cd9adb4ff13eb1d4a046ebef114920ace8
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-02-04 11:24:33 -0500

    Improved No Content Page

--- client/src/components/placeholders/NoContent.vue
+++ client/src/components/placeholders/NoContent.vue
@@ -1,14 +1,20 @@
 <template>
 	<div class="text-center">
-		<h1 class="text-light">{{ text }}</h1>
-		<img :src="require('../../assets/images/blockchain.svg')" class="w-25">
+		<h1 class="text-primary"><AlertCircleIcon size="3x" /></h1>
+		<h1 class="font-weight-bold text-primary">{{ text }}</h1>
 	</div>
 </template>
 
 <script>
+	import { AlertCircleIcon } from 'vue-feather-icons'
+
 	export default {
 		props: {
 			text: { type: String, default: 'there is no content yet' }
-		}
+		},
+
+		components: {
+			AlertCircleIcon
+		},
 	}
 </script>
