796ca0af3a7260ce463d8c4aacd2e56bf81a0172
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-02-08 23:00:31 -0500

    Clean Up Get Quote PAge

--- client/src/pages/components/get-quote.vue
+++ client/src/pages/components/get-quote.vue
@@ -1,13 +1,15 @@
 <template>
 	<BContainer class="my-5">
 		<BRow>
-			<GetQuote />
+			<BCard bg-variant="dark">
+				<GetQuote />
+			</BCard>
 		</BRow>
 	</BContainer>
 </template>
 
 <script>
-	import GetQuote from '../../components/company/GetQuote'
+	import GetQuote from '@/components/company/GetQuote'
 
 	export default {
 		components: {
