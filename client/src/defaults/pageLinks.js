f0829ad97599e6beb499f43c246dffc8dbfe8f22
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-04-14 05:06:17 -0400

    Imporved

--- client/src/defaults/pageLinks.js
+++ client/src/defaults/pageLinks.js
@@ -2,23 +2,28 @@
 const feather = require('feather-icons')
 
 
+// [INIT] const //
+const navIconConfig = {
+	'stroke-width': 3,
+	width: 18,
+	height: 18,
+	'class': ''
+}
+const sideMenuIconConfig = {
+	'stroke-width': 2.4,
+	width: 34,
+	height: 34,
+	'class': 'my-2'
+}
+
+
 // [EXPORT] //
 module.exports = [
 	{
 		path: '/',
 		text: '',
-		navIcon: feather.icons.home.toSvg({
-			'stroke-width': 3,
-			width: 18,
-			height: 18,
-			'class': ''
-		}),
-		sideMenuIcon: feather.icons.home.toSvg({
-			'stroke-width': 2.4,
-			width: 34,
-			height: 34,
-			'class': 'my-2'
-		}),
+		navIcon: feather.icons.home.toSvg(navIconConfig),
+		sideMenuIcon: feather.icons.home.toSvg(sideMenuIconConfig),
 	},
 	{
 		path: '/user/followed/1',
