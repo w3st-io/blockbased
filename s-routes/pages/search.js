19b79137bd1ee380e9752ff660d2c07da490e317
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-04-29 04:38:09 -0400

    Improved

--- s-routes/pages/search.js
+++ s-routes/pages/search.js
@@ -19,84 +19,97 @@ router.get(
 	'/:query/:type/:limit/:page',
 	Auth.userTokenNotRequired(),
 	async (req, res) => {
-		// [VALIDATE] //
-		if (
-			validator.isAscii(req.params.query) &&
-			Number.isInteger(parseInt(req.params.limit)) &&
-			Number.isInteger(parseInt(req.params.page))
-		) {
-			// [INIT] //
-			const user_id = (req.user_decoded) ? req.user_decoded.user_id : undefined
-			const limit = parseInt(req.params.limit)
-			const pageIndex = parseInt(req.params.page) - 1
-			const skip = pageIndex * limit
+		try {
+			// [VALIDATE] //
+			if (
+				validator.isAscii(req.params.query) &&
+				validator.isAscii(req.params.type) &&
+				Number.isInteger(parseInt(req.params.limit)) &&
+				Number.isInteger(parseInt(req.params.page))
+			) {
+				// [INIT] //
+				const user_id = (req.user_decoded) ? req.user_decoded.user_id : undefined
+				const limit = parseInt(req.params.limit)
+				const pageIndex = parseInt(req.params.page) - 1
+				const skip = pageIndex * limit
 
-			// [COUNT] Posts //
-			const { count: postCount } = await postsCollection.c_fuzzySearchCount(
-				req.params.query,
-			)
-
-			// [COUNT] Users //
-			const { count: userCount } = await usersCollection.c_fuzzySearchCount(
-				req.params.query
-			)
-
-			if (req.params.type == 'posts') {
-				// [READ] Posts //
-				const { posts } = await postsCollection.c_fuzzySearch(
-					user_id,
+				// [COUNT] Posts //
+				const { count: postCount } = await postsCollection.c_fuzzySearchCount(
 					req.params.query,
-					limit,
-					skip,
 				)
 
-				// [CALCULATE] Total Pages //
-				const totalPages = Math.ceil(postCount / limit)
-
-				res.send({
-					executed: true,
-					status: true,
-					postResults: posts,
-					postCount: postCount,
-					userCount: userCount,
-					totalPages: totalPages,
-				})
-
-			}
-			else if (req.params.type == 'users') {
-				// [READ] Users //
-				const { users } = await usersCollection.c_fuzzySearch(
-					user_id,
-					req.params.query,
-					limit,
-					skip,
+				// [COUNT] Users //
+				const { count: userCount } = await usersCollection.c_fuzzySearchCount(
+					req.params.query
 				)
 
-				// [CALCULATE] Total Pages //
-				const totalPages = Math.ceil(userCount / limit)
+				if (req.params.type == 'posts') {
+					// [READ] Posts //
+					const { posts } = await postsCollection.c_fuzzySearch(
+						user_id,
+						req.params.query,
+						limit,
+						skip,
+					)
 
-				res.send({
-					executed: true,
-					status: true,
-					userResults: users,
-					postCount: postCount,
-					userCount: userCount,
-					totalPages: totalPages,
-				})
+					// [CALCULATE] Total Pages //
+					const totalPages = Math.ceil(postCount / limit)
+
+					res.send({
+						executed: true,
+						status: true,
+						postResults: posts,
+						postCount: postCount,
+						userCount: userCount,
+						totalPages: totalPages,
+					})
+
+				}
+				else if (req.params.type == 'users') {
+					// [READ] Users //
+					const { users } = await usersCollection.c_fuzzySearch(
+						user_id,
+						req.params.query,
+						limit,
+						skip,
+					)
+
+					// [CALCULATE] Total Pages //
+					const totalPages = Math.ceil(userCount / limit)
+
+					res.send({
+						executed: true,
+						status: true,
+						userResults: users,
+						postCount: postCount,
+						userCount: userCount,
+						totalPages: totalPages,
+					})
+				}
+				else {
+					res.send({
+						executed: true,
+						status: false,
+						location: '/pages/search',
+						message: 'Invalid type',
+					})
+				}
 			}
 			else {
 				res.send({
 					executed: true,
 					status: false,
-					message: 'Invalid type'
+					location: '/pages/search',
+					message: 'Invalid Params',
 				})
 			}
 		}
-		else {
+		catch (err) {
 			res.send({
-				executed: true,
+				executed: false,
 				status: false,
-				message: 'Invalid Params'
+				location: '/pages/search',
+				message: `Caught Error --> ${err}`
 			})
 		}
 	}
