2dfdde54c88d92acae83161604e55afb83cfbd85
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-08-18 01:08:33 -0400

    Improved

--- s-collections/bansCollection.js
+++ s-collections/bansCollection.js
@@ -6,128 +6,123 @@ const mongoose = require('mongoose')
 const BanModel = require('../s-models/BanModel')
 
 
-/******************* [CRUD] *******************/
-// [CREATE] //
-const c_create = async (user_id, hours) => {
-	try {
-		// [VALIDATE] user_id //
-		if (!mongoose.isValidObjectId(user_id)) {
-			return {
-				executed: true,
-				status: false,
-				message: 'bansCollection: Invalid user_id'
+module.exports = {
+	/******************* [CRUD] *******************/
+	// [CREATE] //
+	c_createasync: (user_id, hours) => {
+		try {
+			// [VALIDATE] user_id //
+			if (!mongoose.isValidObjectId(user_id)) {
+				return {
+					executed: true,
+					status: false,
+					message: 'bansCollection: Invalid user_id'
+				}
 			}
-		}
-
-		// [VALIDATE] hours //
-		if (!Number.isInteger(hours)) {
-			return {
-				executed: true,
-				status: false,
-				message: 'bansCollection: Invalid hours (must be numeric)'
+	
+			// [VALIDATE] hours //
+			if (!Number.isInteger(hours)) {
+				return {
+					executed: true,
+					status: false,
+					message: 'bansCollection: Invalid hours (must be numeric)'
+				}
 			}
-		}
-
-		let banTime = new Date()
-		banTime.setHours(banTime.getHours() + hours)
-
-		const createdBan = await new BanModel({
-			_id: mongoose.Types.ObjectId(),
-			user: user_id,
-			bannedTill: banTime,
-		}).save()
-
-		return {
-			executed: true,
-			status: true,
-			createdBan: createdBan,
-		}
-	}
-	catch (err) {
-		return {
-			executed: false,
-			status: false,
-			message: `bansCollection: Error --> ${err}`,
-		}
-	}
-}
-
-
-/******************* [OTHER-CRUD] *******************/
-// [DELETE] //
-const c_deleteByUser = async (user_id) => {
-	try {
-		// [VALIDATE] user_id //
-		if (!mongoose.isValidObjectId(user_id)) {
-			return {
-				executed: true,
-				status: false,
-				message: 'bansCollection: Invalid user_id'
-			}
-		}
-
-		const ban = await BanModel.deleteMany({ user: user_id })
-
-		return {
-			executed: true,
-			status: true,
-			ban: ban,
-		}
-	}
-	catch (err) {
-		return {
-			executed: false,
-			status: false,
-			message: `bansCollection: Error --> ${err}`,
-		}
-	}
-}
-
-
-/******************* [EXISTANCE] *******************/
-// [EXISTANCE] //
-const c_existance = async (user_id) => {
-	try {
-		// [VALIDATE] user_id //
-		if (!mongoose.isValidObjectId(user_id)) {
-			return {
-				executed: true,
-				status: false,
-				message: 'bansCollection: Invalid user_id',
-			}
-		}
-
-		const foundBan = await BanModel.findOne({ user: user_id })
-
-		if (!foundBan) {
+	
+			let banTime = new Date()
+			banTime.setHours(banTime.getHours() + hours)
+	
+			const createdBan = await new BanModel({
+				_id: mongoose.Types.ObjectId(),
+				user: user_id,
+				bannedTill: banTime,
+			}).save()
+	
 			return {
 				executed: true,
 				status: true,
-				message: 'Ban does NOT exists',
-				existance: false,
+				createdBan: createdBan,
 			}
 		}
-
-		return {
-			executed: true,
-			status: true,
-			message: 'Ban does exists',
-			existance: true,
-			foundBan: foundBan,
+		catch (err) {
+			return {
+				executed: false,
+				status: false,
+				message: `bansCollection: Error --> ${err}`,
+			}
 		}
-	}
-	catch (err) {
-		return {
-			executed: false,
-			status: false,
-			message: `banCollection: Error --> ${err}`,
+	},
+
+
+	/******************* [OTHER-CRUD] *******************/
+	// [DELETE] //
+	c_deleteByUser: async (user_id) => {
+		try {
+			// [VALIDATE] user_id //
+			if (!mongoose.isValidObjectId(user_id)) {
+				return {
+					executed: true,
+					status: false,
+					message: 'bansCollection: Invalid user_id'
+				}
+			}
+	
+			const ban = await BanModel.deleteMany({ user: user_id })
+	
+			return {
+				executed: true,
+				status: true,
+				ban: ban,
+			}
 		}
-	}
-}
+		catch (err) {
+			return {
+				executed: false,
+				status: false,
+				message: `bansCollection: Error --> ${err}`,
+			}
+		}
+	},
 
 
-module.exports = {
-	c_create,
-	c_deleteByUser,
-	c_existance
+	/******************* [EXISTANCE] *******************/
+	// [EXISTANCE] //
+	c_existance: async (user_id) => {
+		try {
+			// [VALIDATE] user_id //
+			if (!mongoose.isValidObjectId(user_id)) {
+				return {
+					executed: true,
+					status: false,
+					message: 'bansCollection: Invalid user_id',
+				}
+			}
+	
+			const foundBan = await BanModel.findOne({ user: user_id })
+	
+			if (!foundBan) {
+				return {
+					executed: true,
+					status: true,
+					message: 'Ban does NOT exists',
+					existance: false,
+				}
+			}
+	
+			return {
+				executed: true,
+				status: true,
+				message: 'Ban does exists',
+				existance: true,
+				foundBan: foundBan,
+			}
+		}
+		catch (err) {
+			return {
+				executed: false,
+				status: false,
+				message: `banCollection: Error --> ${err}`,
+			}
+		}
+	},
 }
--- s-collections/commentLikesCollection.js
+++ s-collections/commentLikesCollection.js
@@ -6,322 +6,312 @@ const mongoose = require('mongoose')
 const CommentLikeModel = require('../s-models/CommetLikeModel')
 
 
-/******************* [CRUD] *******************/
-// [CREATE] //
-const c_create = async ({ user_id, post_id, comment_id, commentUser_id }) => {
-	try {
-		// [VALIDATE] user_id //
-		if (!mongoose.isValidObjectId(user_id)) {
-			return {
-				executed: true,
-				status: false,
-				message: 'commentLikesCollection: Invalid user_id',
+module.exports = {
+	/******************* [CRUD] *******************/
+	// [CREATE] //
+	c_create: async ({ user_id, post_id, comment_id, commentUser_id }) => {
+		try {
+			// [VALIDATE] user_id //
+			if (!mongoose.isValidObjectId(user_id)) {
+				return {
+					executed: true,
+					status: false,
+					message: 'commentLikesCollection: Invalid user_id',
+				}
 			}
-		}
-
-		// [VALIDATE] post_id //
-		if (!mongoose.isValidObjectId(post_id)) {
-			return {
-				executed: true,
-				status: false,
-				message: 'commentLikesCollection: Invalid post_id',
-			}
-		}
-
-		// [VALIDATE] comment_id //
-		if (!mongoose.isValidObjectId(comment_id)) {
-			return {
-				executed: true,
-				status: false,
-				message: 'commentLikesCollection: Invalid comment_id',
-			}
-		}
-
-		// [VALIDATE] commentUser_id //
-		if (!mongoose.isValidObjectId(commentUser_id)) {
-			return {
-				executed: true,
-				status: false,
-				message: 'commentLikesCollection: Invalid commentUser_id',
-			}
-		}
 	
-		// [SAVE] //
-		const commentLike = await new CommentLikeModel({
-			_id: mongoose.Types.ObjectId(),
-			user: user_id,
-			post: post_id,
-			comment: comment_id,
-			commentUser: commentUser_id,
-		}).save()
-
-		return {
-			executed: true,
-			status: true,
-			commentLike: commentLike,
-		}
-	}
-	catch (err) {
-		return {
-			executed: false,
-			status: false,
-			message: `commentLikesCollection: Error --> ${err}`,
-		}
-	}
-}
-
-
-/******************* [OTHER-CRUD] *******************/
-// [DELETE] post //
-const c_deleteByPost = async (post_id) => {
-	try {
-		// [VALIDATE] post_id //
-		if (!mongoose.isValidObjectId(post_id)) {
-			return {
-				executed: true,
-				status: false,
-				message: 'commentLikesCollection: Invalid post_id',
+			// [VALIDATE] post_id //
+			if (!mongoose.isValidObjectId(post_id)) {
+				return {
+					executed: true,
+					status: false,
+					message: 'commentLikesCollection: Invalid post_id',
+				}
 			}
-		}
-
-		const commentLikes = await CommentLikeModel.deleteMany({ post: post_id })
-
-		return {
-			executed: true,
-			status: true,
-			commentLikes: commentLikes
-		}
-	}
-	catch (err) {
-		return {
-			executed: true,
-			status: false,
-			message: `commentLikesCollection: Error --> ${err}`,
-		}
-	}
-}
-
-
-// [DELETE] comment //
-const c_deleteByComment = async (comment_id) => {
-	try {
-		// [VALIDATE] comment_id //
-		if (!mongoose.isValidObjectId(comment_id)) {
-			return {
-				executed: true,
-				status: false,
-				message: 'commentLikesCollection: Invalid comment_id',
+	
+			// [VALIDATE] comment_id //
+			if (!mongoose.isValidObjectId(comment_id)) {
+				return {
+					executed: true,
+					status: false,
+					message: 'commentLikesCollection: Invalid comment_id',
+				}
 			}
-		}
-
-		const commentLikes = await CommentLikeModel.deleteMany({ comment: comment_id })
-
-		return {
-			executed: true,
-			status: true,
-			commentLikes: commentLikes
-		}
-	}
-	catch (err) {
-		return {
-			executed: true,
-			status: false,
-			message: `commentLikesCollection: Error --> ${err}`,
-		}
-	}
-}
-
-
-// [DELETE] user & comment //
-const c_deleteByUserAndComment = async ({ user_id, comment_id }) => {
-	try {
-		// [VALIDATE] user_id //
-		if (!mongoose.isValidObjectId(user_id)) {
-			return {
-				executed: true,
-				status: false,
-				message: 'commentLikesCollection: Invalid user_id',
+	
+			// [VALIDATE] commentUser_id //
+			if (!mongoose.isValidObjectId(commentUser_id)) {
+				return {
+					executed: true,
+					status: false,
+					message: 'commentLikesCollection: Invalid commentUser_id',
+				}
 			}
-		}
-
-		// [VALIDATE] comment_id //
-		if (!mongoose.isValidObjectId(comment_id)) {
-			return {
-				executed: true,
-				status: false,
-				message: 'commentLikesCollection: Invalid comment_id',
-			}
-		}
-
-		const commentLike = await CommentLikeModel.deleteMany({
-			user: user_id,
-			comment: comment_id,
-		})
-
-		return {
-			executed: true,
-			status: true,
-			commentLike: commentLike,
-		}
-	}
-	catch (err) {
-		return {
-			executed: false,
-			status: false,
-			message: `commentLikesCollection: Error --> ${err}`,
-		}
-	}
-}
-
-
-// [DELETE] Custom //
-const c_deleteCustom = async (filter) => {
-	try {
-		// [VALIDATE] filter //
-		if (!filter || filter == {}) {
-			return {
-				executed: true,
-				status: false,
-				message: 'commentLikesCollection: No filter passed',
-				updated: false,
-			}
-		}
-
-		const commentLike = await CommentLikeModel.deleteMany(filter)
-
-		return {
-			executed: true,
-			status: true,
-			commentLike: commentLike,
-		}
-	}
-	catch (err) {
-		return {
-			executed: false,
-			status: false,
-			message: `commentLikesCollection: Error --> ${err}`,
-		}
-	}
-}
-
-
-/******************* [EXISTANCE] *******************/
-// [EXISTANCE] //
-const c_existance = async ({ user_id, comment_id }) => {
-	try {
-		// [VALIDATE] user_id //
-		if (!mongoose.isValidObjectId(user_id)) {
-			return {
-				executed: true,
-				status: false,
-				message: 'commentLikesCollection: Invalid user_id',
-			}
-		}
-
-		// [VALIDATE] comment_id //
-		if (!mongoose.isValidObjectId(comment_id)) {
-			return {
-				executed: true,
-				status: false,
-				message: 'commentLikesCollection: Invalid comment_id',
-			}
-		}
-
-		if (!await CommentLikeModel.findOne({ user: user_id, comment: comment_id,})) {
+		
+			// [SAVE] //
+			const commentLike = await new CommentLikeModel({
+				_id: mongoose.Types.ObjectId(),
+				user: user_id,
+				post: post_id,
+				comment: comment_id,
+				commentUser: commentUser_id,
+			}).save()
+	
 			return {
 				executed: true,
 				status: true,
-				message: 'commentLike does NOT exist',
-				existance: false,
+				commentLike: commentLike,
 			}
 		}
-
-		return {
-			executed: true,
-			status: true,
-			message: 'commentLike does exist',
-			existance: true,
+		catch (err) {
+			return {
+				executed: false,
+				status: false,
+				message: `commentLikesCollection: Error --> ${err}`,
+			}
 		}
-	}
-	catch (err) {
-		return {
-			executed: false,
-			status: false,
-			message: `commentLikesCollection: Error --> ${err}`,
+	},
+
+
+	/******************* [OTHER-CRUD] *******************/
+	// [DELETE] post //
+	c_deleteByPost: async (post_id) => {
+		try {
+			// [VALIDATE] post_id //
+			if (!mongoose.isValidObjectId(post_id)) {
+				return {
+					executed: true,
+					status: false,
+					message: 'commentLikesCollection: Invalid post_id',
+				}
+			}
+	
+			const commentLikes = await CommentLikeModel.deleteMany({ post: post_id })
+	
+			return {
+				executed: true,
+				status: true,
+				commentLikes: commentLikes
+			}
 		}
-	}
-}
-
-
-/******************* [COUNT] *******************/
-const c_countByComment = async (comment_id) => {
-	try {
-		// [VALIDATE] comment_id //
-		if (!mongoose.isValidObjectId(comment_id)) {
+		catch (err) {
 			return {
 				executed: true,
 				status: false,
-				message: 'commentLikesCollection: Invalid comment_id',
+				message: `commentLikesCollection: Error --> ${err}`,
 			}
 		}
+	},
 
-		const count = await CommentLikeModel.countDocuments({ comment: comment_id })
 
-		return {
-			executed: true,
-			status: true,
-			count: count
+	// [DELETE] comment //
+	c_deleteByComment: async (comment_id) => {
+		try {
+			// [VALIDATE] comment_id //
+			if (!mongoose.isValidObjectId(comment_id)) {
+				return {
+					executed: true,
+					status: false,
+					message: 'commentLikesCollection: Invalid comment_id',
+				}
+			}
+	
+			const commentLikes = await CommentLikeModel.deleteMany({ comment: comment_id })
+	
+			return {
+				executed: true,
+				status: true,
+				commentLikes: commentLikes
+			}
 		}
-	}
-	catch (err) {
-		return {
-			executed: false,
-			status: false,
-			message: `commentLikesCollection: Error --> ${err}`,
-		}
-	}
-}
-
-
-const c_countByCommentUser = async (commentUser_id) => {
-	try {
-		// [VALIDATE] commentUser_id //
-		if (!mongoose.isValidObjectId(commentUser_id)) {
+		catch (err) {
 			return {
 				executed: true,
 				status: false,
-				message: 'commentLikesCollection: Invalid commentUser_id',
+				message: `commentLikesCollection: Error --> ${err}`,
 			}
 		}
+	},
+
+
+	// [DELETE] user & comment //
+	c_deleteByUserAndComment: async ({ user_id, comment_id }) => {
+		try {
+			// [VALIDATE] user_id //
+			if (!mongoose.isValidObjectId(user_id)) {
+				return {
+					executed: true,
+					status: false,
+					message: 'commentLikesCollection: Invalid user_id',
+				}
+			}
 	
-		const count = await CommentLikeModel.countDocuments({
-			commentUser: commentUser_id
-		})
-
-		return {
-			executed: true,
-			status: true,
-			count: count
-		}
-	}
-	catch (err) {
-		return {
-			executed: false,
-			status: false,
-			message: `commentLikesCollection: Error --> ${err}`
-		}
-	}
+			// [VALIDATE] comment_id //
+			if (!mongoose.isValidObjectId(comment_id)) {
+				return {
+					executed: true,
+					status: false,
+					message: 'commentLikesCollection: Invalid comment_id',
+				}
+			}
 	
-}
+			const commentLike = await CommentLikeModel.deleteMany({
+				user: user_id,
+				comment: comment_id,
+			})
+	
+			return {
+				executed: true,
+				status: true,
+				commentLike: commentLike,
+			}
+		}
+		catch (err) {
+			return {
+				executed: false,
+				status: false,
+				message: `commentLikesCollection: Error --> ${err}`,
+			}
+		}
+	},
 
 
-module.exports = {
-	c_create,
-	c_deleteByPost,
-	c_deleteByComment,
-	c_deleteByUserAndComment,
-	c_deleteCustom,
-	c_existance,
-	c_countByComment,
-	c_countByCommentUser,
+	// [DELETE] Custom //
+	c_deleteCustom: async (filter) => {
+		try {
+			// [VALIDATE] filter //
+			if (!filter || filter == {}) {
+				return {
+					executed: true,
+					status: false,
+					message: 'commentLikesCollection: No filter passed',
+					updated: false,
+				}
+			}
+	
+			const commentLike = await CommentLikeModel.deleteMany(filter)
+	
+			return {
+				executed: true,
+				status: true,
+				commentLike: commentLike,
+			}
+		}
+		catch (err) {
+			return {
+				executed: false,
+				status: false,
+				message: `commentLikesCollection: Error --> ${err}`,
+			}
+		}
+	},
+
+
+	/******************* [EXISTANCE] *******************/
+	// [EXISTANCE] //
+	c_existance: async ({ user_id, comment_id }) => {
+		try {
+			// [VALIDATE] user_id //
+			if (!mongoose.isValidObjectId(user_id)) {
+				return {
+					executed: true,
+					status: false,
+					message: 'commentLikesCollection: Invalid user_id',
+				}
+			}
+	
+			// [VALIDATE] comment_id //
+			if (!mongoose.isValidObjectId(comment_id)) {
+				return {
+					executed: true,
+					status: false,
+					message: 'commentLikesCollection: Invalid comment_id',
+				}
+			}
+	
+			if (!await CommentLikeModel.findOne({ user: user_id, comment: comment_id,})) {
+				return {
+					executed: true,
+					status: true,
+					message: 'commentLike does NOT exist',
+					existance: false,
+				}
+			}
+	
+			return {
+				executed: true,
+				status: true,
+				message: 'commentLike does exist',
+				existance: true,
+			}
+		}
+		catch (err) {
+			return {
+				executed: false,
+				status: false,
+				message: `commentLikesCollection: Error --> ${err}`,
+			}
+		}
+	},
+
+
+	/******************* [COUNT] *******************/
+	c_countByComment: async (comment_id) => {
+		try {
+			// [VALIDATE] comment_id //
+			if (!mongoose.isValidObjectId(comment_id)) {
+				return {
+					executed: true,
+					status: false,
+					message: 'commentLikesCollection: Invalid comment_id',
+				}
+			}
+	
+			const count = await CommentLikeModel.countDocuments({ comment: comment_id })
+	
+			return {
+				executed: true,
+				status: true,
+				count: count
+			}
+		}
+		catch (err) {
+			return {
+				executed: false,
+				status: false,
+				message: `commentLikesCollection: Error --> ${err}`,
+			}
+		}
+	},
+
+
+	c_countByCommentUser: async (commentUser_id) => {
+		try {
+			// [VALIDATE] commentUser_id //
+			if (!mongoose.isValidObjectId(commentUser_id)) {
+				return {
+					executed: true,
+					status: false,
+					message: 'commentLikesCollection: Invalid commentUser_id',
+				}
+			}
+		
+			const count = await CommentLikeModel.countDocuments({
+				commentUser: commentUser_id
+			})
+	
+			return {
+				executed: true,
+				status: true,
+				count: count
+			}
+		}
+		catch (err) {
+			return {
+				executed: false,
+				status: false,
+				message: `commentLikesCollection: Error --> ${err}`
+			}
+		}
+		
+	},
 }
