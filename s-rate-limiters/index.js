7fb8a915b8fd081b1b3fb14dab3a617a5bde1e7a
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-08-28 15:35:15 -0400

    Improved

--- s-rate-limiters/index.js
+++ s-rate-limiters/index.js
@@ -6,96 +6,87 @@ const rateLimit = require('express-rate-limit')
 const defaultMessage = 'Too many requests, please try again later'
 
 
-// [RATE-LIMIT] Global //
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 500, // limit each IP to 100 requests per windowMs
-	message: {
-		executed: true,
-		status: false,
-		message: defaultMessage,
-	}
-})
-
-
-// [RATE-LIMIT] Post //
-const postLimiter = rateLimit({
-	windowMs: 60 * 60 * 1000,
-	max: 60,
-	message: {
-		executed: true,
-		status: false,
-		message: defaultMessage,
-	}
-})
-
-
-// [RATE-LIMIT] Comment //
-const commentLimiter = rateLimit({
-	windowMs: 60 * 60 * 1000,
-	max: 60,
-	message: {
-		executed: true,
-		status: false,
-		message: defaultMessage,
-	}
-})
-
-
-// [RATE-LIMIT] Follow //
-const followLimiter = rateLimit({
-	windowMs: 60 * 60 * 1000,
-	max: 100,
-	message: {
-		executed: true,
-		status: false,
-		message: defaultMessage,
-	}
-})
-
-
-// [RATE-LIMIT] Like //
-const likeLimiter = rateLimit({
-	windowMs: 60 * 60 * 1000,
-	max: 200,
-	message: {
-		executed: true,
-		status: false,
-		message: defaultMessage,
-	}
-})
-
-
-// [RATE-LIMIT] Registration //
-const registrationLimiter = rateLimit({
-	windowMs: 60 * 60 * 1000,
-	max: 20,
-	message: {
-		executed: true,
-		status: false,
-		message: defaultMessage,
-	}
-})
-
-
-// [RATE-LIMIT] report //
-const reportLimiter = rateLimit({
-	windowMs: 60 * 60 * 1000,
-	max: 10,
-	message: {
-		executed: true,
-		status: false,
-		message: defaultMessage,
-	}
-})
-
-
 module.exports = {
-	limiter,
-	postLimiter,
-	commentLimiter,
-	followLimiter,
-	likeLimiter,
-	registrationLimiter,
-	reportLimiter,
+	// [GLOBAL] //
+	limiter: rateLimit({
+		windowMs: 15 * 60 * 1000, // 15 minutes
+		max: 500,
+		message: {
+			executed: true,
+			status: false,
+			message: defaultMessage,
+		}
+	}),
+
+
+	// [POST] //
+	postLimiter: rateLimit({
+		windowMs: 60 * 60 * 1000,
+		max: 60,
+		message: {
+			executed: true,
+			status: false,
+			message: defaultMessage,
+		}
+	}),
+
+
+	// [COMMENT] //
+	commentLimiter: rateLimit({
+		windowMs: 60 * 60 * 1000,
+		max: 60,
+		message: {
+			executed: true,
+			status: false,
+			message: defaultMessage,
+		}
+	}),
+
+
+	// [FOLLOW] //
+	followLimiter: rateLimit({
+		windowMs: 60 * 60 * 1000,
+		max: 100,
+		message: {
+			executed: true,
+			status: false,
+			message: defaultMessage,
+		}
+	}),
+
+
+	// [LIKE] //
+	likeLimiter: rateLimit({
+		windowMs: 60 * 60 * 1000,
+		max: 200,
+		message: {
+			executed: true,
+			status: false,
+			message: defaultMessage,
+		}
+	}),
+
+
+	// [REGISTRATION] //
+	registrationLimiter: rateLimit({
+		windowMs: 60 * 60 * 1000,
+		max: 20,
+		message: {
+			executed: true,
+			status: false,
+			message: defaultMessage,
+		}
+	}),
+
+
+	// [REPORT] //
+	reportLimiter: rateLimit({
+		windowMs: 60 * 60 * 1000,
+		max: 10,
+		message: {
+			executed: true,
+			status: false,
+			message: defaultMessage,
+		}
+	}),
 }
